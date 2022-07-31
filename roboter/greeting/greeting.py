ac814b6403d98173af1f3b612d2049c6e20b9212
enginearn <enginyaaaaan@gmail.com>
2022-07-31 23:47:03 +0900

    Added additiona

--- roboter/greeting/greeting.py
+++ roboter/greeting/greeting.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-
+import re
 
 class GreetAndQuestions(object):
     def __init__(self, your_name="anonymous", my_name="roboter", restaurant="somewhere nice place"):
@@ -32,11 +32,42 @@ class GreetAndQuestions(object):
         print(f"{self.your_name} Thank you!\nHave a nice meal at {self.restaurant}!")
         # return self.restaurant
 
-        def recommend_restaurant(self):
-            print(f"{self.your_name} I recommend {self.restaurant}!\nDo you like it?")
+    def recommend_restaurant(self):
+        # empty_counter = 0
+        res = input(f"{self.your_name} I recommend {self.restaurant}!\nDo you like it? [yes/no]")
+        # while res == "" or empty_counter < 3:
+        #     res = input("I couldn't hear you...\nDo you like it? [yes/no]")
+        #     empty_counter += 1
+        #     if empty_counter == 3:
+        #         print(f"I have asked {empty_counter} times!")
+        #         print("I'm sorry, I can't understand you...")
+        #         continue
+        #     continue
+        res = res.lower()
+        no_counter = 0
+        while no_counter < 3:
+            if re.match(r"^(yes|y)$", res):
+                print("Thank you!")
+                break
+            elif re.match(r"^(no|n)$", res):
+                res = input(f"How about this one? {self.restaurant} [yes/no]")
+                no_counter += 1
+                if no_counter == 3:
+                    print("I'll try to recommend you a nice restaurant again some day...")
+                    break
+            elif res == "":
+                res = input("I couldn't hear you...\nDo you like it? [yes/no]")
+                no_counter += 1
+                if no_counter == 3:
+                    print("I'll try to recommend you a nice restaurant again some day...")
+                    break
+                continue
+            else:
+                print("I'll try to recommend you a nice restaurant again some day...")
 
-# greeting_a = GreetAndQuestions()
-# greeting_a.say_hello()
+greeting_a = GreetAndQuestions()
+greeting_a.say_hello()
+greeting_a.recommend_restaurant()
 # user_name = greeting_a.your_name
 # print(f"You are {user_name}, aren't you?")
 # greeting_a.ask_restaurant()
